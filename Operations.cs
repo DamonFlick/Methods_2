ff7f44fce0babbd3be347152af00679a8877806c
DamonFlick <75501983+DamonFlick@users.noreply.github.com>
2021-02-22 18:45:35 -0800

    Add project files.

---
+++ Operations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_2
+{
+    class Operations
+    {
+        public static int opr(int x)
+        {
+            return x - 2;
+        }
+
+        public static int opr(double x)
+        {
+            return Convert.ToInt32(x * 2);
+        }
+
+        public static int opr(string x)
+        {
+            try
+            {
+                int y = Convert.ToInt32(x);
+                return Convert.ToInt32(y / 2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+                return 0;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an interger to subtract two from it");
+            Console.WriteLine( "Result: " + Operations.opr(Convert.ToInt32(Console.ReadLine())));
+            Console.ReadLine();
+
+            Console.WriteLine("Enter a decimal number multiply by two");
+            Console.WriteLine("Result: " + Operations.opr(Convert.ToDouble(Console.ReadLine())));
+            Console.ReadLine();
+
+            Console.WriteLine(" Enter a number to be converted to int by a custom method, then divided by two");
+            Console.WriteLine("Result: " + Operations.opr(Console.ReadLine()));
+            Console.ReadLine();
+        }
+    }
+}
